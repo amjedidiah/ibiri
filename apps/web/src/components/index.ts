8ae363aebcc05989c9bf705e91d87b1076ba4023
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-09-25 15:45:07 +0100

    refactor(app): review for feature/auth-pages

--- apps/web/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Loader from './Loader';
-import LoginForm from './LoginForm';
-import RegisterForm from './RegisterForm';
-import { withAuth } from './withAuth';
-
-export { Loader, LoginForm, RegisterForm, withAuth };
